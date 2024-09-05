bafb9c74cc02733881951aad8d72bfa698b76dfa
Logeswaran <logeswaran.sr@hotmail.com>
2024-09-05 23:42:46 +0530

    Website Design - Final Update
    Final Update for Portfolio Website in Design

--- src/components/StaticData.js
+++ src/components/StaticData.js
@@ -1,11 +1,13 @@
 // import HeroSectionImage from "../assets/images/HeroSectionImage.jpg"
 import Project2048 from "../assets/images/Project2048 Screenshot.png"
 import EDA from "../assets/images/EDA Screenshot 2.png"
-import AIChaperone from "../assets/images/AI Chaperone - App Logo.png" 
+import AIChaperone from "../assets/images/AI Chaperone - App Logo.jpg" 
 import AquaSense from "../assets/images/AquaSense - App Logo.png"
 import MineAI from "../assets/images/MineAI logo.png"
 import React from "../assets/images/React Icon.png"
 import Flask from "../assets/images/Flask Logo.png"
+import Flutter from "../assets/images/Flutter Logo.png"
+import Firebase from "../assets/images/Firebase Logo.png"
 import PyTorch from "../assets/images/Pytorch Logo.png"
 import Tensorflow from "../assets/images/Tensorflow Logo.png"
 import Django from "../assets/images/Django Logo.png"
@@ -19,6 +21,7 @@ import VEI from "../assets/images/VEI Technologies.jpg"
 import Prodigy from "../assets/images/Prodigy Infotech.jpg"
 import Andiran from "../assets/images/Andiran HealthTech Logo (Without Text).jpg"
 import ThinkTank from "../assets/images/srinivasa_ramanuja_think_tank_logo.jpg"
+import Zoho from "../assets/images/Zoho Logo.png"
 import LeetCode from "../assets/images/Leetcode Logo.png"
 import LinkedIn from "../assets/images/Linkedin Logo.png"
 import HuggingFace from "../assets/images/Huggingface Logo.png"
@@ -89,12 +92,22 @@ export const SkillsData = {
             logo: Django
         },
         {
+            name: "SQL",
+            logo: SQL
+        },
+    ],
+    "Mobile App Development":[
+        {
             name: "React Native",
             logo: React
         },
         {
-            name: "SQL",
-            logo: SQL
+            name: "Flutter",
+            logo: Flutter
+        },
+        {
+            name: "Firebase",
+            logo: Firebase
         },
     ],
     "AI & ML": [
@@ -134,33 +147,40 @@ export const SkillsData = {
 export const ExperienceData = {
     "Internship":[
         {
+            role:"Project Intern",
+            company:"Andiran Healthtech Pvt. Ltd.",
+            period:"10/06/2024 - 09/07/2024",
+            desc:"Offline Internship, Contributed to developing an application with Flutter and Firebase.",
+            img: Andiran
+        },
+        {
             role: "ML Intern",
             company: "Prodigy Infotech",
             period: "15/12/2023 - 15/01/2024",
-            desc: "Sample Text",
+            desc: "Online Internship as a Machine Learning Intern; Worked hands-on with Machine Learning models.",
             img: Prodigy
         },
         {
             role: "AI Intern",
             company: "VEI Technologies",
             period: "26/07/2023 - 09/08/2023",
-            desc: "Sample Text",
+            desc: "Offline Internship - gained insights on usage of AI in Industry.",
             img: VEI
         },
     ],
     "Current":[
         {
-            role:"Project Intern",
-            company:"Andiran Healthtech Pvt. Ltd.",
-            start_date:"10/06/2024",
-            desc:"Sample Text",
-            img: Andiran
+            role:"Project Trainee",
+            company:"Zoho Corporation Pvt. Ltd.",
+            start_date:"08/2024",
+            desc:"Front-end Developer in Zoho Expense Team.",
+            img: Zoho
         },
         {
             role:"Research Assistant",
             company:"The Srinivasa Ramanuja Think Tank",
             start_date:"01/2024",
-            desc:"Sample Text",
+            desc:"Research Assistant, in-charge of Deep Learning Team.",
             img:ThinkTank
         }
     ]
